453d131964f62c1c3f79fb2be3a3bc484e7e804f
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2025-01-12 00:25:33 -0500

    improve touch feel for touchable button

--- src/lib/components/TouchableButton.svelte
+++ src/lib/components/TouchableButton.svelte
@@ -8,43 +8,66 @@
 	export let buttonClassName = '';
 
 	let isPressed = false;
-	let animationTimeout: NodeJS.Timeout;
+	let isHovered = false;
+	let isClicked = false;
 
-	function handlePress() {
-		isPressed = true;
-	}
-
-	function handleRelease() {
-		isPressed = false;
-		// Clear any existing timeout
-		if (animationTimeout) clearTimeout(animationTimeout);
-		// Set a new timeout to remove the animation class
-		animationTimeout = setTimeout(() => {
-			isPressed = false;
-		}, 300); // Slightly longer than our animation duration
+	$: {
+		if (isClicked) {
+			setTimeout(() => {
+				isClicked = false;
+			}, 300);
+		}
 	}
 
 	function handleKeyDown(e: KeyboardEvent) {
 		if (e.key === 'Enter' || e.key === ' ') {
 			e.preventDefault();
 			e.stopPropagation();
-			handlePress();
+			isClicked = true;
 			onClick();
-			// Simulate release after a short delay
-			setTimeout(handleRelease, 100);
+		}
+	}
+
+	function handleEvent(e: Event) {
+		e.stopPropagation();
+		// e.preventDefault();
+		console.log(e.type);
+		if (e.type === 'click' || e.type === 'contextmenu') {
+			isClicked = true;
+			onClick();
+			e.preventDefault();
+		}
+		if (e.type === 'pointerenter') {
+			isHovered = true;
+		}
+		if (e.type === 'pointerdown') {
+			isPressed = true;
+		}
+		if (e.type === 'pointerup') {
+			isPressed = false;
+		}
+		if (e.type === 'pointerleave') {
+			isHovered = false;
+			isPressed = false;
 		}
 	}
 </script>
 
+<!-- svelte-ignore a11y_mouse_events_have_key_events -->
 <div
-	class="touchable-button flex cursor-pointer items-center p-2 transition-all duration-75 ease-out hover:scale-110 hover:brightness-110 focus-visible:scale-110 focus-visible:brightness-110 {disabled
+	class="touchable-button flex cursor-pointer items-center p-2 transition-all duration-75 ease-out focus-visible:scale-110 focus-visible:brightness-110 {disabled
 		? 'pointer-events-none opacity-50'
-		: ''} {isPressed ? 'press-down' : ''} {!isPressed ? 'pop-back' : ''} {className}"
-	on:click|stopPropagation={onClick}
+		: ''} {isHovered ? 'scale-110 brightness-110' : ''} {isPressed ? 'scale-95' : ''} {isClicked
+		? 'pop-back'
+		: ''}
+		 {className}"
 	on:keydown={handleKeyDown}
-	on:pointerdown={handlePress}
-	on:pointerup={handleRelease}
-	on:pointerleave={handleRelease}
+	on:click={handleEvent}
+	on:pointerenter={handleEvent}
+	on:pointerleave={handleEvent}
+	on:pointerdown={handleEvent}
+	on:pointerup={handleEvent}
+	on:contextmenu={handleEvent}
 	role="button"
 	tabindex={disabled ? -1 : 0}
 	aria-label={ariaLabel}
@@ -78,11 +101,6 @@
 		outline: none !important;
 	}
 
-	.press-down {
-		transform: scale(1);
-		transition: transform 0.075s ease-out;
-	}
-
 	.pop-back {
 		animation: pop-back 0.2s ease-out;
 	}
@@ -92,7 +110,7 @@
 			transform: scale(0.95);
 		}
 		50% {
-			transform: scale(1.15);
+			transform: scale(1.2);
 		}
 		100% {
 			transform: scale(1.1);
